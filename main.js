287dd2b35dd2b61c1470bab3e99733a85bf6616b
Abdurakhman <abdurvhman1@mail.ru>
2022-11-09 11:45:14 +0300

    Тернарный оператор

--- main.js
+++ main.js
@@ -1,23 +1,5 @@
-'use strict'
+"use strict"
 
-let numberOfFilms = +prompt('Сколько фильмов вы посмотрели ?', '')
+// Тернарный оператор
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {
-        
-    },
-    actors: {},
-    genres: [],
-    private: false
-}
-
-const a = prompt('Один из последих просмотренних фильмов ?'),
-    b = prompt('На сколько оцените его ?'),
-    c = prompt('Один из последих просмотренних фильмов ?'),
-    d = prompt('На сколько оцените его ?')
-
-personalMovieDB.movies[a] = b
-personalMovieDB.movies[c] = d
-
-console.log(personalMovieDB.movies);
+5 < 2 ? console.log('ok') : 5 > 1 ? console.log('yes') : console.log('false');
