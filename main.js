c21037e6dd8aa4ad9dd2d05242eff12a45b23c40
Abdurakhman <abdurvhman1@mail.ru>
2022-11-09 11:48:34 +0300

    Тернарный оператор и свич

--- main.js
+++ main.js
@@ -3,3 +3,20 @@
 // Тернарный оператор
 
 5 < 2 ? console.log('ok') : 5 > 1 ? console.log('yes') : console.log('false');
+
+
+let num = 10
+
+switch (num) {
+    case 1:
+        console.log('true1');
+        break;
+    case 2:
+        console.log('true2');
+        break;
+    case 10:
+        console.log('true3');
+        break;
+    default:
+        break;
+}
